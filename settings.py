1465e64c4bb5fb199841fdc1803777b4d2004c9c
Vincent Ly <vincentavly@hotmail.com>
2022-01-25 19:12:25 -0500

    Settings cleanup

--- settings.py
+++ settings.py
@@ -1,11 +1,3 @@
-# Files
-road_file = "test3.pickle"
-seed = 7
-func = "dj"
-
-# 75374827393
-
-# PArking error: 93438333, num_cars: 123
 # Screen
 width = 1100
 height = 650
@@ -20,17 +12,15 @@ move_speed = 4
 
 scale_factor = 1
 scale_speed = 0.025
-max_scale = 2.4  # 2.4
+max_scale = 2.4
 min_scale = 0.6
 
-# Method2 Variables
 # IMPORTANT: IF YOU CHANGE THEM, ADJUST THEM IN METHOD2
 adjust_x = 1100  # move_x
 adjust_y = 700  # move_y
 road_len = 52
 
 # Cars
-num_car = 300  # 300
 car_len = 8
 car_speed = 0.5
 min_dist = car_len * 0.3125
@@ -38,8 +28,7 @@ min_dist = car_len * 0.3125
 park_dist = 9.89707610702726
 park_time = 32
 
-# Useful info
-# Gas: 32__  Distance Traveled: 10__ Distance Road: 8__
+
 road_center = 4
 
 # Time
@@ -53,3 +42,9 @@ car_evolution = []
 
 count = 0
 tested = 3
+
+# YOU CAN CHANGE THESE SETTINGS_______________________________________________
+num_car = 300
+road_file = "test3.pickle"
+seed = 7
+func = "dj"
